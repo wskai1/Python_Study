dbb606483e61c3cd2ba92211603aa4be3e656c4a
wskai1 <1729253813@qq.com>
2018-08-02 15:57:39 +0800

    add mysite content

---
+++ mysite/polls/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+
+from polls import views
+app_name = 'polls'  # 关键是这行
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+]
