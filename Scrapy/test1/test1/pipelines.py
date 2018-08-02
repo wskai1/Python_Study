26cb00e277cca656311cacf0fec153607bb47260
wskai1 <1729253813@qq.com>
2018-08-02 16:34:32 +0800

    xiugai

--- Scrapy/test1/test1/pipelines.py
+++ Scrapy/test1/test1/pipelines.py
@@ -4,8 +4,23 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import codecs
+import json
+import os
 
 
 class Test1Pipeline(object):
+    def __init__(self):
+        self.file = codecs.open('items.json', 'w', encoding='utf-8')
+        self.file.write('[')
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(line + ',')
         return item
+
+    def close_spider(self, spider):
+        self.file.seek(-1, os.SEEK_END)
+        self.file.truncate();
+        self.file.write(']')
+        self.file.close()
